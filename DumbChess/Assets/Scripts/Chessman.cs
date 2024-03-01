db1d8f4c90992e29802221b382188711e1a3fb01
Brandon Cherry <brandon.cherry@mymail.champlain.edu>
2024-03-01 16:38:37 -0500

    Tutorial 3 finished

--- DumbChess/Assets/Scripts/Chessman.cs
+++ DumbChess/Assets/Scripts/Chessman.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Chessman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject controller;
+    public GameObject movePlate;
+
+    //Positions
+    private int xBoard = -1;
+    private int yBoard = -1;
+
+    private string player; //Keeps track of black and white player 
+
+    public Sprite black_queen, black_knight, black_bishop, black_rook, black_pawn, black_king;
+    public Sprite white_queen, white_knight, white_bishop, white_rook, white_pawn, white_king;
+
+    public void Activate() //Brandon
     {
-        
+        controller = GameObject.FindGameObjectWithTag("GameController");
+        SetCoords();
+
+        switch(this.name) //Set the sprite for the piece
+        {
+            //Black
+            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            //White
+            case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+            case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+            case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+            case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+            case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+            case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCoords() //Brandon
     {
-        
+        float x = xBoard;
+        float y = yBoard;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
     }
 }
