7edc363bac2efb13680d6722f224a3a6c5199236
Wreckitshoot <90283922+Wreckitshoot@users.noreply.github.com>
2024-03-05 10:34:52 -0500

    Tutorial part 5

---
+++ DumbChess/Assets/Scripts/MovePlate.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject controller;
+
+    GameObject reference = null;
+
+
+    int matrixX;
+    int matrixY;
+
+
+    public bool attack = false;
+
+    public void Start()
+    {
+        if(attack)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        if(attack )
+        {
+            GameObject cp = controller.GetComponent<Game>().GetPosition(matrixY, matrixX);
+            Destroy(cp);
+            controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(),reference.GetComponent<Chessman>().GetYBoard());
+
+            reference.GetComponent<Chessman>().SetXBoard(matrixX);
+            reference.GetComponent<Chessman>().SetYBoard(matrixY);
+            reference.GetComponent<Chessman>().SetCoords();
+
+            controller.GetComponent<Game>().SetPosition(reference);
+
+            reference.GetComponent<Chessman>().DestroyMovePlates();
+        }
+    }
+
+    public void setCoords(int x,int y)
+    {
+        matrixX = x;
+        matrixY = y;
+    }
+
+    public void SetReference(GameObject obj)
+    {
+        reference = obj;
+    }
+
+    public GameObject GetReference()
+    {
+        return reference;
+    }
+}
